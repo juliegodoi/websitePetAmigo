52790a6f7ab04bd3ebb6c7ad1fb616a21aabbd8b
Julie <juliegodoi@gmail.com>
2023-12-12 11:00:03 -0300

    update fix commit

--- script.js
+++ script.js
@@ -35,3 +35,30 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 1);
 }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var lazySections = document.querySelectorAll('.lazy-section');
+
+    function lazyLoadSection(section) {
+    if (isElementInViewport(section)) {
+        section.classList.add('loaded');
+    }
+}
+
+    function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return rect.top <= window.innerHeight && rect.bottom >= 0;
+}
+
+    function handleLazyLoad() {
+    lazySections.forEach(function (section) {
+        lazyLoadSection(section);
+    });
+}
+
+  handleLazyLoad(); // Carregue as seções que são visíveis inicialmente
+
+    window.addEventListener('scroll', function () {
+    handleLazyLoad();
+});
+});
